03fa30ecc5b6400b520466b9525b2dd20ef781e3
kalyanbathula <135245013+kalyanbathula@users.noreply.github.com>
2024-07-14 23:07:54 +0530

    Update PromptSuggestionsRow.tsx

--- components/PromptSuggestions/PromptSuggestionsRow.tsx
+++ components/PromptSuggestions/PromptSuggestionsRow.tsx
@@ -2,11 +2,17 @@ import PromptSuggestionButton from "./PromptSuggestionButton";
 
 const PromptSuggestionRow = ({ onPromptClick }) => {
 const prompts = [
-    'How does DigiCrop perform crop analysis?',
-    'What features does DigiCrop offer for monitoring crop health?',
-    'How does DigiCrop help in identifying crop diseases?',
-    'How can farmers use DigiCrop to improve crop yield?',
-    'What types of crop data can be accessed through DigiCrop?',
+  //  'How does DigiCrop perform crop analysis?',
+    //'What features does DigiCrop offer for monitoring crop health?',
+    //'How does DigiCrop help in identifying crop diseases?',
+    //'How can farmers use DigiCrop to improve crop yield?',
+    //'What types of crop data can be accessed through DigiCrop?',
+
+     ' What are the steps to prepare mango pulp?',
+    'Tell me about Paddy Crop Calendar?',
+    'Crop Calendar of Sesame?',
+    'What Factors Contribute to Indias dominant position as a supplier of moringa globally?',
+    'What are the essential steps to manage stem borer infestation in paddy from nursery preparation to harvesting?'
     
 ];
 
